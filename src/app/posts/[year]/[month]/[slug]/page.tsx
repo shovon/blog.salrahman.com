1091627f8f22e02d9146e25f27660e8c18abff3a
Sal Rahman <hey.to.sal@gmail.com>
2025-09-20 19:52:33 -0700

    Updated metadata

--- src/app/posts/[year]/[month]/[slug]/page.tsx
+++ src/app/posts/[year]/[month]/[slug]/page.tsx
@@ -1,35 +1,54 @@
-"use client";
-
 import { allPosts } from "content-collections";
 import { useParams, notFound } from "next/navigation";
 import { generatePath } from "@/lib/article-meta";
 import { format } from "date-fns";
+import type { Metadata } from "next";
 
-export default function Home() {
-	const params = useParams();
+type Params = { year: string; month: string; slug: string };
 
-	const { year, month, slug } = params;
+type MetadataProps = {
+  params: Promise<Params>;
+};
 
-	const post = allPosts.find(
-		(post) => `/${year}/${month}/${slug}` === generatePath(post)
-	);
+export async function generateMetadata({ params }: MetadataProps) {
+  const { year, month, slug } = await params;
 
-	if (!post) {
-		notFound();
-	}
+  const post = allPosts.find(
+    (post) => `/${year}/${month}/${slug}` === generatePath(post)
+  );
 
-	return (
-		<main className="my-0 mx-auto mb-12 max-w-3xl mt-11">
-			<article>
-				<hgroup className="mb-4">
-					<p className="text-gray-500">
-						{format(post.publishDate, "MMMM do, y")}
-					</p>
-					<h1 className="text-5xl mb-3 font-bold">{post.title}</h1>
-					<p className="text-gray-600">{post.summary}</p>
-				</hgroup>
-				<section dangerouslySetInnerHTML={{ __html: post.html }}></section>
-			</article>
-		</main>
-	);
+  if (!post) {
+    return { title: "Blog" };
+  }
+
+  return {
+    title: post.title,
+  };
+}
+
+export default function BlogPost({ params }: { params: Params }) {
+  const { year, month, slug } = params;
+
+  const post = allPosts.find(
+    (post) => `/${year}/${month}/${slug}` === generatePath(post)
+  );
+
+  if (!post) {
+    notFound();
+  }
+
+  return (
+    <main className="my-0 mx-auto mb-12 max-w-3xl mt-11">
+      <article>
+        <hgroup className="mb-4">
+          <p className="text-gray-500">
+            {format(post.publishDate, "MMMM do, y")}
+          </p>
+          <h1 className="text-5xl mb-3 font-bold">{post.title}</h1>
+          <p className="text-gray-600">{post.summary}</p>
+        </hgroup>
+        <section dangerouslySetInnerHTML={{ __html: post.html }}></section>
+      </article>
+    </main>
+  );
 }
